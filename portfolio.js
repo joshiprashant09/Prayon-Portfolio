22a6a0ba9fb3aeb75422e1903f8dd301b5e08c51
Prashant Joshi <zocpj10@gmail.com>
2024-07-20 17:35:27 +0530

    "Updated portfolio.js and style.css files with new event listeners, styles, and animations for scrolling, navigation, and profile image effects."

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,15 @@
-document.querySelector('.scroll-arrow').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#about').scrollIntoView({
-        behavior: 'smooth'
-    });
+document.getElementById('scroll-to-about').addEventListener('click', function() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
 });
+
+document.getElementById('scroll-to-contact').addEventListener('click', function() {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('scroll-to-contact2').addEventListener('click', function() {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
+
+setTimeout(function() {
+    document.querySelector('.profile-image').style.animation = 'none';
+}, 6000);
