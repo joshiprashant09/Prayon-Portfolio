db13592d5792bfd596f590d493218373515ea88a
Prashant Joshi <zocpj10@gmail.com>
2024-07-23 18:10:54 +0530

    Update javascript

--- portfolio.js
+++ portfolio.js
@@ -13,3 +13,20 @@ document.getElementById('scroll-to-contact2').addEventListener('click', function
 setTimeout(function() {
     document.querySelector('.profile-image').style.animation = 'none';
 }, 6000);
+ 
+function sendEmail(){
+    Email.send({
+        SecureToken : "e4031e14-d8e9-445b-abc2-4385bd656b32",
+        To : 'prasantnov9@gmail.com',
+        From : "prasantnov9@gmail.com",
+        Subject : "Contact Form Enquiry",
+        Body : "Name" + document.getElementById("name").value
+            + "<br> Email: " +document.getElementById("email").value
+            + "<br> Phone: " +document.getElementById("phone").value
+            + "<br> Message: " +document.getElementById("message").value
+    }).then(
+      message => alert(message)
+    );
+    
+}
+
